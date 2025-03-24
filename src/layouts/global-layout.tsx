3f520248e2591aec404e018ccfa9747e6a8944ed
yuwol <yuwol036@gmail.com>
2025-03-24 21:21:14 +0900

    feat: Add breakpoints for responsive design

--- src/layouts/global-layout.tsx
+++ src/layouts/global-layout.tsx
@@ -2,11 +2,14 @@ import type { FC } from 'react'
 import { Outlet } from 'react-router'
 import { Footer } from '@/components/footer/footer'
 import { Header } from '@/components/header/header'
+import styles from './global-layout.module.scss'
 
 export const GlobalLayout: FC = () => (
   <>
     <Header />
-    <Outlet />
+    <div className={styles.responsiveWrapper}>
+      <Outlet />
+    </div>
     <Footer />
   </>
 )
