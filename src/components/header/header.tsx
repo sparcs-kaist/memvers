b787d6a64149d0a9be87c22e018f7c7ad39ffc33
yuwol <yuwol036@gmail.com>
2025-03-24 21:21:47 +0900

    feat: Enhance header component with logo and login link

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,14 @@
 import type { FC } from 'react'
+import { Link } from 'react-router'
 import styles from './header.module.scss'
 
-export const Header: FC = () => {
-  return <header className={styles.header}>Header</header>
-}
+export const Header: FC = () => (
+  <div className={styles.headerWrapper}>
+    <header className={styles.header}>
+      <Link to="/" className={styles.logo}>
+        <img src="/images/memvers.svg" alt="Memvers" />
+      </Link>
+      <Link to="/login">Login</Link>
+    </header>
+  </div>
+)
