a49147710ac7db09da556c4540ba1429d5e14b4f
yuwol <yuwol036@gmail.com>
2025-04-03 17:10:29 +0900

    refactor: Move router setup into providers

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router'
 import { QueryProvider } from '@/providers/query'
-import { router } from '@/router'
+import { RouterProvider } from '@/providers/router'
 import '@/styles/index.scss'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryProvider>
-      <RouterProvider router={router} />
+      <RouterProvider />
     </QueryProvider>
   </StrictMode>,
 )
