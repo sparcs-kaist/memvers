0b9d7f489c52b822b570548112fa5e71eb153e62
yuwol <yuwol036@gmail.com>
2025-04-04 16:50:10 +0900

    feat: Configure Vite API proxy for development

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,32 @@
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
-import { defineConfig } from 'vite'
+import { type UserConfig, defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vite.dev/config/
-export default defineConfig({
+const baseConfig: UserConfig = {
   plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
       '@styles': path.resolve(__dirname, './src/styles'),
     },
   },
-})
+}
+
+const devConfig: UserConfig = {
+  ...baseConfig,
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.VITE_API_PROXY_TARGET,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+}
+
+const prodConfig: UserConfig = baseConfig
+
+export default defineConfig(({ command }) =>
+  command === 'serve' ? devConfig : prodConfig,
+)
